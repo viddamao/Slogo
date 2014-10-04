ca64ac8ee85515649c352586fd3c89d3ece86efd
Wenjun Mao <wm56@duke.edu>
2014-10-04 19:14:10 -0400

    modified API design, added token recognizer and turtle model

--- src/controller/Compiler.java
+++ src/controller/Compiler.java
@@ -1,48 +1,25 @@
 package controller;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-
 import exceptions.ParsingException;
 
 public class Compiler {
-    private InfoSheet myInfoSheet = new InfoSheet();
-    private ResourceBundle ourProperties;
+    private static Compiler myCompiler;
 
-    @SuppressWarnings("resource")
-    private void initializeKeywordTable() throws ParsingException {
-
-	List<String> keywordTable = new ArrayList<>();
-	ourProperties = ResourceBundle.getBundle("messages", Locale.US);
-	/*Scanner s = new Scanner(new BufferedReader(new FileReader(
-		ourProperties.getString("keywordTablePath"))));
-	while (s.hasNext()) {
-	    keywordTable.add(s.next());
-	}*/
-	myInfoSheet.setKeywordTable(keywordTable);
-
-    }
-
+   
+	
+   
     private String scanner(String inputBuffer) throws ParsingException {
-
+	String[] inputSplit = inputBuffer.split(" ");
+	System.out.println(inputSplit[1]);
+	System.out.println(TokenFinder.tokenize(inputSplit)[1]);
 	return inputBuffer;
     }
 
-    private void driver() throws ParsingException {
-	scanner("ab c");
 
-    }
-
-    public static void main(String[] args) throws ParsingException {
-
-	Compiler myCompiler = new Compiler();
-	myCompiler.initializeKeywordTable();
-	myCompiler.driver();
+    public static void main(String[] args) throws Exception {
+	String inputString = "fd 50";
+	myCompiler = new Compiler();
+	myCompiler.scanner(inputString);
     }
 
 }
---
+++ src/simulationObjects/Point.java
@@ -0,0 +1,40 @@
+package simulationObjects;
+
+public class Point{
+	
+	private double x;
+	private double y;
+	
+	/**
+	 * defines what is a point
+	 * @param x
+	 * @param y
+	 */
+	public Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * init the point at 0 , 0
+	 */
+	public Point(){
+		this(0,0);
+	}
+	
+	/**
+	 * get x value of point 
+	 * @return x value
+	 */
+	public double getX(){
+		return x;
+	}
+	
+	/**
+	 * get y value of point
+	 * @return y value
+	 */
+	public double getY(){
+		return y;
+	}
+}
