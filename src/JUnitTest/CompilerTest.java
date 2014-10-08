c646a117bd119c6e365ee72a634aae895963bc8b
Wenjun Mao <wm56@duke.edu>
2014-10-08 18:58:00 -0400

    modified compiler, added parse Table

--- src/JUnitTest/CompilerTest.java
+++ src/JUnitTest/CompilerTest.java
@@ -10,32 +10,32 @@ import org.junit.rules.ExpectedException;
 
 public class CompilerTest {
 
-    Compiler compiler=new Compiler();
-  	@Rule
-	public ExpectedException exception = ExpectedException.none();
-	@Test
-	public void testGrammarException() {
+    Compiler compiler = new Compiler();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void testGrammarException() {
 	exception.expect(IllegalArgumentException.class);
 	exception.expectMessage("Grammatically Error");
-	//* is not an acceptable variable name
-	String testInput="fd *";
+	// * is not an acceptable variable name
+	String testInput = "fd *";
 	compiler.compile(testInput);
-	}
-	
-	@Test
-	public void testSyntaxException() {
+    }
+
+    @Test
+    public void testSyntaxException() {
 	exception.expect(IllegalArgumentException.class);
 	exception.expectMessage("Syntactically Error");
-	String testInput="fd 12 13";
+	String testInput = "fd 12 13";
 	compiler.compile(testInput);
-	}
-	
-	@Test
-	public void testScanner(){
-	    String testInput="fd 12 13";    
-	    Type[] tokens= compiler.compile(testInput);
-	    assertEquals(tokens[0],Type.COMMAND);
-	}
-	
-    
+    }
+
+    @Test
+    public void testScanner() {
+	String testInput = "fd 12 13";
+	Type[] tokens = compiler.compile(testInput);
+	assertEquals(tokens[0], Type.COMMAND);
+    }
+
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -7,12 +7,13 @@ public class MainController {
 
     private Compiler compiler = new Compiler();
 
-    public String passInput(String input) throws ParsingException,
-	    UnbalancedBracketsException {
-
-	return compiler.compile(input);
+    public void initialize() {
 
     }
-    
+
+    public void parseInput(String input) throws ParsingException,
+	    UnbalancedBracketsException {
+
+    }
 
 }
