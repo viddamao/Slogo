2790f1ddd4aa39138c98c3402d862a4e601cf80a
Kevin Rhine <kevin.rhine@duke.edu>
2014-10-21 21:56:34 -0400

    turtle top, grid bottom
    Turtle on top of grid

--- src/gui/ColorPicker.java
+++ src/gui/ColorPicker.java
@@ -20,82 +20,69 @@ import javax.swing.JOptionPane;
  *
  * Allows user to set colors for simulation cells
  *
- * @author Viddamao
- *
+ * @author Wenjun
+ * @author Kevin Rhine
  */
 public class ColorPicker extends javafx.scene.control.ColorPicker {
-    private GridInfo infoSheet = new GridInfo();
-    private Grid grid;
+	private GridInfo infoSheet = new GridInfo();
+	private Grid grid;
 
-    public ColorPicker(Grid g) {
-	grid = g;
-    }
+	public ColorPicker(Grid g) {
+		grid = g;
+	}
 
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    private GridPane buildColorPickerMenu(int index, GridPane gridpane) {
-	final ColorPicker colorPicker = new ColorPicker(grid);
-	final Text text = new Text(infoSheet.getStateTypes().get(index));
-	text.setFont(Font.font("Verdana", 20));
-	String stateType = text.getText();
-	Color myColor = infoSheet.getColor(stateType);
-	colorPicker.setValue(myColor);
-	text.setFill(myColor);
-
-	colorPicker.setOnAction(new EventHandler() {
-
-	    @Override
-	    public void handle(Event t) {
-
-		Color myColor = colorPicker.getValue();
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private GridPane buildColorPickerMenu(Text text, GridPane gridpane) {
+		final ColorPicker colorPicker = new ColorPicker(grid);
+		text.setFont(Font.font("Verdana", 20));
+		String stateType = text.getText();
+		Color myColor = infoSheet.getColor(stateType);
+		colorPicker.setValue(myColor);
 		text.setFill(myColor);
-		infoSheet.setColor(stateType, myColor);
 
-		if ((text.getText() == "BACKGROUND")
-			| (text.getText().contains("_"))) {
-		    grid.updateBackgroundColor(myColor);
+		colorPicker.setOnAction(new EventHandler() {
+
+			@Override
+			public void handle(Event t) {
+
+				Color myColor = colorPicker.getValue();
+				text.setFill(myColor);
+				infoSheet.setColor(stateType, myColor);
+
+				if ((text.getText() == "BACKGROUND")
+						| (text.getText().contains("_"))) {
+					grid.updateBackgroundColor(myColor);
+				}
+			}
+		});
+
+		gridpane.add(colorPicker, 1, index);
+		gridpane.add(text, 2, index);
+
+		return gridpane;
+
+	}
+
+
+	public void showColorPicker() {
+		Stage myStage = new Stage();
+		myStage.setResizable(false);
+		myStage.setTitle("Choose colors for the simulation!");
+		GridPane gridpane = new GridPane();
+		gridpane.getColumnConstraints().add(new ColumnConstraints(60));
+		gridpane.getColumnConstraints().add(new ColumnConstraints(90));
+		Text[] colorsToChange = {new Text("Background"),new Text("Pen")};
+		for (int i = 0; i < colorsToChange.length; i++) {
+			gridpane.getRowConstraints().add(new RowConstraints(35));
 		}
-	    }
-	});
 
-	gridpane.add(colorPicker, 1, index);
-	gridpane.add(text, 2, index);
+		Scene scene = new Scene(gridpane, 400, 40 * colorsToChange.length);
 
-	return gridpane;
+		for (int i = 0; i < colorsToChange.length; i++) {
+			gridpane = buildColorPickerMenu(colorsToChange[i], gridpane);
+		}
 
-    }
-
-    private void sendPickFileAlert() {
-	ResourceBundle messages = ResourceBundle.getBundle("messages",
-		Locale.US);
-	JOptionPane.showMessageDialog(null,
-		messages.getString("missing_sim_type"));
-    }
-
-    public void showColorPicker() {
-	Stage myStage = new Stage();
-	myStage.setResizable(false);
-	myStage.setTitle("Choose colors for the simulation!");
-	GridPane gridpane = new GridPane();
-	int rowNumber = infoSheet.getStateTypes().size();
-	gridpane.getColumnConstraints().add(new ColumnConstraints(60));
-	gridpane.getColumnConstraints().add(new ColumnConstraints(90));
-	for (int i = 0; i < rowNumber; i++) {
-	    gridpane.getRowConstraints().add(new RowConstraints(35));
+		myStage.setScene(scene);
+		myStage.show();
 	}
-
-	if (infoSheet.getStateTypes() == null) {
-	    sendPickFileAlert();
-	    return;
-	}
-
-	Scene scene = new Scene(gridpane, 400, 40 * rowNumber);
-
-	for (int i = 0; i < infoSheet.getStateTypes().size(); i++) {
-	    gridpane = buildColorPickerMenu(i, gridpane);
-
-	}
-
-	myStage.setScene(scene);
-	myStage.show();
-    }
 }
